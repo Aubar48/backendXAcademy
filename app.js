a9551cb04936536ae866fee41e57a8024341349a
“Nahuel <nnahuell.gb@hotmail.com>
2024-10-23 05:28:53 -0300

    Add .env file and database connection

--- app.js
+++ app.js
@@ -4,35 +4,26 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 let cors = require('cors');
-let sequelize = require('./config/database');
-let indexRouter = require('./routes/player');
-let usersRouter = require('./routes/users');
-
 const sequelize = require('./src/config/dabase');
-const Player = require('./src/./models/player');
+const Player = require('./src/models/player'); // Asegúrate de que esté correctamente escrito
+
 var app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json()); // Para parsear JSON en las solicitudes
-
-
 app.use(logger('dev'));
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -43,23 +34,21 @@ app.use(function(err, req, res, next) {
 });
 
 // Verificar conexión a la base de datos
-  async function initialize() {
-    try {
-      await sequelize.authenticate();
-      console.log('Conexión con la base de datos establecida correctamente.');
-      
-      // Sincronizar el modelo con la base de datos
-      await Player.sync();
-      console.log('El modelo Player está sincronizado con la tabla `players`.');
-  
-      // Aquí puedes agregar lógica adicional, como consultas a la base de datos
-    } catch (error) {
-      console.error('No se pudo conectar a la base de datos:', error);
-    } finally {
-      await sequelize.close();
-    }
+async function initialize() {
+  try {
+    await sequelize.authenticate();
+    console.log('Conexión con la base de datos establecida correctamente.');
+
+    // Sincronizar el modelo con la base de datos
+    await Player.sync();
+    console.log('El modelo Player está sincronizado con la tabla `players`.');
+
+    // Aquí puedes agregar lógica adicional, como consultas a la base de datos
+  } catch (error) {
+    console.error('No se pudo conectar a la base de datos:', error);
   }
-  
-  initialize();
+}
+
+initialize();
 
 module.exports = app;
